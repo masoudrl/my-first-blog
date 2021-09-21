c5bd05de248fbf8ca003cb698b2345b7411d6eb4
masoudrl <masoudrahimi777@gmail.com>
2021-09-21 17:03:19 +0430

    f3

---
+++ blog/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path, include
+from . import views
+urlpatterns = [
+    path('',views.post_list,name='post_list'),
+    path('date',views.view_date,name='view_date')
+]
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
-
-# Create your views here.
+from django.http import HttpResponse
+from django.utils import timezone
+def post_list(request):
+    return HttpResponse("Hello Django")
+def view_date(request):
+    return HttpResponse(str(timezone.now()))
